041daf6f1b90d490c1bdcd2a131a14a2ca3ff3ef
Senorhun <petersz.andrassy@gmail.com>
2026-03-09 22:07:06 +0100

    add employee + restaurant

---
+++ restaurant/src/main/java/com/example/restaurant/model/Restaurant.java
@@ -0,0 +1,22 @@
+package com.example.restaurant.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int capacity;
+    private int star;
+
+    @OneToMany(mappedBy = "restaurant")
+    private List<Employee> employeeList;
+
+}
