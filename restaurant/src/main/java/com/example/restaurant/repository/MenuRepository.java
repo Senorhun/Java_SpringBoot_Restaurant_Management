cbf60aca2abf3446f99c1db5edde2edb8e984494
Senorhun <petersz.andrassy@gmail.com>
2026-04-07 14:41:24 +0200

    feat: add available menu item pagination

--- restaurant/src/main/java/com/example/restaurant/repository/MenuRepository.java
+++ restaurant/src/main/java/com/example/restaurant/repository/MenuRepository.java
@@ -2,11 +2,12 @@ package com.example.restaurant.repository;
 
 import com.example.restaurant.model.MenuItem;
 import com.example.restaurant.model.MenuItemType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
@@ -15,4 +16,6 @@ public interface MenuRepository extends JpaRepository<MenuItem,Long> {
     List<MenuItem> getByType(@Param("type") MenuItemType type);
 
     boolean existsByNameIgnoreCase(String name);
+    Page<MenuItem> findByAvailableTrue(Pageable pageable);
+
 }
