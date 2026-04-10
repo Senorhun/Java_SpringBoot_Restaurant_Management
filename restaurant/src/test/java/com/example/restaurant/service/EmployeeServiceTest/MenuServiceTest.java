cb36d096da8521552feb681c2ce62276b12df810
Senorhun <petersz.andrassy@gmail.com>
2026-04-10 23:30:28 +0200

    wip: getByType menuItem

--- restaurant/src/test/java/com/example/restaurant/service/EmployeeServiceTest/MenuServiceTest.java
+++ restaurant/src/test/java/com/example/restaurant/service/EmployeeServiceTest/MenuServiceTest.java
@@ -3,6 +3,7 @@ package com.example.restaurant.service.EmployeeServiceTest;
 import com.example.restaurant.dto.MenuItemCreateCommand;
 import com.example.restaurant.dto.MenuItemInfo;
 import com.example.restaurant.model.MenuItem;
+import com.example.restaurant.model.MenuItemType;
 import com.example.restaurant.model.Restaurant;
 import com.example.restaurant.repository.MenuRepository;
 import com.example.restaurant.service.MenuService;
@@ -13,6 +14,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,7 +24,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MenuServiceTest {
@@ -100,5 +104,35 @@ public class MenuServiceTest {
         assertNotNull(result);
         assertEquals("BurgerGo", result.getFirst().getRestaurantName());
         assertEquals(2, result.size());
+    }
+    @Test
+    void getByType_shouldReturnPagedMenuItemInfo() {
+        MenuItemType type = MenuItemType.DESSERT;
+        Restaurant restaurant = new Restaurant();
+        restaurant.setName("BurgerGo");
+
+        MenuItem menuItem1 = new MenuItem();
+        menuItem1.setRestaurant(restaurant);
+        MenuItem menuItem2 = new MenuItem();
+        menuItem2.setRestaurant(restaurant);
+
+        List<MenuItem> items = new ArrayList<>();
+        items.add(menuItem1);
+        items.add(menuItem2);
+
+        Page<MenuItem> page = new PageImpl<>(items);
+
+        MenuItemInfo menuItemInfo1 = new MenuItemInfo();
+        MenuItemInfo menuItemInfo2 = new MenuItemInfo();
+
+        when(menuRepository.findByMenuItemTypeAndAvailableTrue(eq(MenuItemType.DESSERT), any(Pageable.class))).thenReturn(page);
+        when(modelMapper.map(menuItem1, MenuItemInfo.class)).thenReturn(menuItemInfo1);
+        when(modelMapper.map(menuItem2, MenuItemInfo.class)).thenReturn(menuItemInfo2);
+
+        Page<MenuItemInfo> result = menuService.getByType(type, 0, 10);
+        assertEquals(2, result.getContent().size());
+        assertEquals("BurgerGo", result.getContent().getFirst().getRestaurantName());
+
+        verify(menuRepository).findByMenuItemTypeAndAvailableTrue(eq(type), any(Pageable.class));
     }
 }
