148fd9be306ee39f41163927ef5cf33ed2b9494e
Senorhun <petersz.andrassy@gmail.com>
2026-04-10 19:28:20 +0200

    wip: save test

---
+++ restaurant/src/test/java/com/example/restaurant/service/EmployeeServiceTest/MenuServiceTest.java
@@ -0,0 +1,67 @@
+package com.example.restaurant.service.EmployeeServiceTest;
+
+import com.example.restaurant.dto.MenuItemCreateCommand;
+import com.example.restaurant.dto.MenuItemInfo;
+import com.example.restaurant.exceptionhandling.MenuItemNameDuplicationException;
+import com.example.restaurant.model.MenuItem;
+import com.example.restaurant.model.Restaurant;
+import com.example.restaurant.repository.MenuRepository;
+import com.example.restaurant.service.MenuService;
+import com.example.restaurant.service.RestaurantService;
+import jakarta.validation.Valid;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.awt.*;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class MenuServiceTest {
+
+    @Mock
+    ModelMapper modelMapper;
+
+    @Mock
+    MenuRepository menuRepository;
+
+    @Mock
+    RestaurantService restaurantService;
+
+    @InjectMocks
+    MenuService menuService;
+
+    @Test
+    void save_shouldReturnMenuInfo_whenMenuExists(){
+        MenuItemCreateCommand command = new MenuItemCreateCommand();
+        Long restaurantId = 1L;
+        command.setRestaurantId(restaurantId);
+        command.setName("Burger");
+
+        Restaurant restaurant = new Restaurant();
+        restaurant.setName("BurgerGo");
+
+        MenuItem menuItem = new MenuItem();
+        MenuItem savedMenuItem = new MenuItem();
+        MenuItemInfo menuItemInfo = new MenuItemInfo();
+
+        when(menuRepository.existsByNameIgnoreCase("Burger")).thenReturn(false);
+        when(restaurantService.findById(restaurantId)).thenReturn(restaurant);
+        when(modelMapper.map(command, MenuItem.class)).thenReturn(menuItem);
+        when(menuRepository.save(menuItem)).thenReturn(savedMenuItem);
+        when(modelMapper.map(savedMenuItem, MenuItemInfo.class)).thenReturn(menuItemInfo);
+
+        MenuItemInfo result = menuService.save(command);
+        assertNotNull(result);
+        assertEquals("BurgerGo", result.getRestaurantName());
+
+    }
+
+}
